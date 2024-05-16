1751ffad5e950dd6d53eca6c83144d11f43b5503
Moh. Firdaus <mohammedfirdaus1404@gmail.com>
2024-05-16 11:06:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hitung_gross_up(penghasilan_netto):
 
 def main():
     st.title('Kalkulator Pajak')
-    penghasilan_netto = st.number_input('Masukkan Penghasilan Netto:', min_value=0.0, step=1.0)
+    penghasilan_netto = st.number_input('Masukkan Penghasilan Netto:', min_value=0.0, step=5000.0)
     if st.button('Hitung'):
         penghasilan_bruto_gross_up, dpp, pajak = hitung_gross_up(penghasilan_netto)
         st.write(f'Penghasilan Bruto Gross-Up: Rp{penghasilan_bruto_gross_up:,}')
